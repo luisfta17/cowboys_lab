5ffa91abc08d11bc55c937e74a252036355a0437
Luis Tejero <luisfaridta@gmail.com>
2018-04-24 15:48:18 +0100

    save method working

---
+++ console.rb
@@ -0,0 +1,22 @@
+require("pry")
+require_relative("models/space_cowboys")
+
+cowboy1 = BountyClass.new({
+  "name" => "Han",
+  "species" => "Human",
+  "last_known_location" => "AsteroidAB",
+  "homeworld" => "Earth"
+  })
+cowboy1.save()
+cowboy2 = BountyClass.new({
+  "name" => "Chewbaca",
+  "species" => "Wookie",
+  "last_known_location" => "AsteroidAB",
+  "homeworld" => "Wookie Land"
+  })
+cowboy2.save()
+
+
+  binding.pry
+  nil
+  
--- models/space_cowboys.rb
+++ models/space_cowboys.rb
@@ -12,7 +12,25 @@ class BountyClass
      @homeworld = options["homeworld"]
    end
 
- 
+   def save()
+     db = PG.connect({
+       dbname: "space_cowboys",
+       host: "localhost"
+       })
+    sql = "INSERT INTO space_cowboys
+       (name,
+        species,
+        last_known_location,
+        homeworld)
+        VALUES
+        ($1, $2, $3, $4) RETURNING id"
+    values = [@name, @species, @last_known_location, @homeworld]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]["id"].to_i
+    db.close()
+  end
+
+
 
 
 
